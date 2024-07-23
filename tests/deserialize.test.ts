4cee97fd2ec61e5e8ebf9a9019560fcb7d6590a8
Stuart Hanberg <shanberg@users.noreply.github.com>
2024-07-23 13:07:53 +0000

    cleanup: remove commented tests

--- tests/deserialize.test.ts
+++ tests/deserialize.test.ts
@@ -155,51 +155,7 @@ Deno.test("deserialize - corrects invalid indentation", () => {
   assertEquals(deserialize(input), expected);
 });
 
-// Deno.test("deserialize - adds missing marker", () => {
-//   const input = `
-// - Root Frame
-//   - Child 1
-//     - Grandchild 1.1
-//   - Child 2
-//     - Grandchild 2.1
-// `;
 
-//   const expected = {
-//     key: "0",
-//     name: "Root Frame",
-//     isCurrent: false,
-//     children: [
-//       {
-//         key: "1",
-//         name: "Child 1",
-//         isCurrent: false,
-//         children: [
-//           {
-//             key: "2",
-//             name: "Grandchild 1.1",
-//             isCurrent: true,
-//             children: [],
-//           },
-//         ],
-//       },
-//       {
-//         key: "3",
-//         name: "Child 2",
-//         isCurrent: false,
-//         children: [
-//           {
-//             key: "4",
-//             name: "Grandchild 2.1",
-//             isCurrent: false,
-//             children: [],
-//           },
-//         ],
-//       },
-//     ],
-//   };
-
-//   assertEquals(deserialize(input), expected);
-// });
 
 Deno.test("deserialize - no root node", () => {
   const input = ``;
@@ -211,65 +167,6 @@ Deno.test("deserialize - no root node", () => {
   );
 });
 
-// Deno.test("deserialize - no current item, set first deepest as current", () => {
-//   const input = `
-// - Root
-//   - Child 1
-//     - Grandchild 1.1
-//     - Grandchild 1.2
-//   - Child 2
-//     - Grandchild 2.1
-//     - Grandchild 2.2
-// `;
-
-//   const expected = {
-//     key: "0",
-//     name: "Root",
-//     isCurrent: false,
-//     children: [
-//       {
-//         key: "1",
-//         name: "Child 1",
-//         isCurrent: false,
-//         children: [
-//           {
-//             key: "2",
-//             name: "Grandchild 1.1",
-//             isCurrent: true,
-//             children: [],
-//           },
-//           {
-//             key: "3",
-//             name: "Grandchild 1.2",
-//             isCurrent: false,
-//             children: [],
-//           },
-//         ],
-//       },
-//       {
-//         key: "4",
-//         name: "Child 2",
-//         isCurrent: false,
-//         children: [
-//           {
-//             key: "5",
-//             name: "Grandchild 2.1",
-//             isCurrent: false,
-//             children: [],
-//           },
-//           {
-//             key: "6",
-//             name: "Grandchild 2.2",
-//             isCurrent: false,
-//             children: [],
-//           },
-//         ],
-//       },
-//     ],
-//   };
-
-//   assertEquals(deserialize(input), expected);
-// });
 
 Deno.test("deserialize - multiple current markers", () => {
   const input = `- Root
@@ -281,14 +178,3 @@ Deno.test("deserialize - multiple current markers", () => {
     'Multiple items marked as current at line: "  - Child 2 @"',
   );
 });
-
-// Deno.test("deserialize - invalid indentation", () => {
-//   const input = `- Root
-//     - Child 1
-//   - Child 2`;
-//   assertThrows(
-//     () => deserialize(input),
-//     Error,
-//     'Invalid indentation at line: "  - Child 2"',
-//   );
-// });
